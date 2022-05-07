802715e0bea5ad0d4eb8ab1bfc56c3da8d7af79b
frylzy <fanruyi199608@163.com>
2022-05-07 22:56:09 +0800

    first commit

---
+++ test1.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+const englishHelloPrefix = "Hello, "
+
+func main() {
+	fmt.Println(Hello(""))
+}
+
+func Hello(name string) string {
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
+}
